842a6f87701af7cb719cc25ad78b404430ebf4b2
Qi <53514723+Nedasd@users.noreply.github.com>
2024-08-23 11:49:51 +0200

    add output/error path

--- example/sbatch.sh
+++ example/sbatch.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 #SBATCH -t 4-00:00:00              # time limit: (D-HH:MM:SS) 
-#SBATCH --job-name=Qi_run          # job name, "Qi_run"
+#SBATCH --job-name=example          # job name, "Qi_run"
 #SBATCH --ntasks=1                 # each individual task in the job array will have a single task associated with it
 #SBATCH --mem-per-cpu=8G		       # Memory Request (per CPU; can use on GLIC)
 
-source /your/path/for/miniforge3/bin/activate
+#SBATCH --output /your/path/to/output/output.txt
+#SBATCH --error /your/path/to/output/error.txt
+
+source /your/path/to/output/miniforge3/bin/activate
 conda activate seismic
 
-run python /your/python/file/path/test.py
+srun python /your/path/to/output/read_data_from_Glic.py
